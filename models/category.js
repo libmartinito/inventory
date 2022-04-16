552a7dff66f35a26e242fe01a21a37d8ffa7ffb3
lib <lib.martinito@pm.me>
2022-04-16 14:59:59 +0800

    set up data models

---
+++ models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+// virtual for the category's URL
+CategorySchema.virtual("url").get(function () {
+  return "/" + this._id;
+});
+
+// export model
+module.exports = mongoose.model("Category", CategorySchema);
---
+++ models/item.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ItemSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: [{ type: Schema.Types.ObjectId, ref: "Category" }],
+  price: { type: Number, required: true },
+  numberInStock: { type: Number, required: true },
+});
+
+// virtual property for a book's URL
+ItemSchema.virtual("url").get(function () {
+  return "/" + this._id;
+});
+
+// export model
+module.exports = mongoose.model("Item", ItemSchema);
